de5039484f78e2a89ef3f2055ea88e553f37e2da
ayaazzam24 <ayaazzam0101@gmail.com>
2021-01-27 20:23:05 +0200

    modified file

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 alert('welcome to my page')
-var user = prompt('you are male or female');
+var user = prompt('are you male or female?');
     if(user == 'female'){
         alert ('weclome to women perfumes')
     }else if (user == 'male'){
         alert('Hello, I hope you will like the page')
     }
-
+document.body.style.backgroundColor = 'pink';
 var aya = prompt ('what is your name')
 alert('hello' + aya )
+
+
+
+
 
 
 
