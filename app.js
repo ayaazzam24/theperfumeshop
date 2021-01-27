47983c14c772508bd662bdbf39abe9d84d812d66
ayaazzam24 <ayaazzam0101@gmail.com>
2021-01-27 16:54:25 +0200

    modified file and added new file

---
+++ app.js
@@ -0,0 +1,26 @@
+alert('welcome to my page')
+var user = prompt('you are male or female');
+    if(user == 'female'){
+        alert ('weclome to women perfumes')
+    }else if (user == 'male'){
+        alert('Hello, I hope you will like the page')
+    }
+
+var aya = prompt ('what is your name')
+alert('hello' + aya )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
