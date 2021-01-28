af281a8f6f4b8cb5d8c1d23941f417f712b8b44d
ayaazzam24 <ayaazzam0101@gmail.com>
2021-01-28 22:07:32 +0200

    modified on file

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 
+var perfumeType = prompt ('welcome to my shop , What kind of perfume do you want to shop ,Si or One Million ? ')
 
-
-
-
-var perfumeType = prompt ('welcome to my shop ,what you want to shop ,si or one million ')
-
-while( perfumeType !== 'si'&& perfumeType !=='one million'){
+while( perfumeType !== 'Si'&& perfumeType !=='One Million'){
      perfumeType = prompt ('please enter the correct item')
 }
 
-var perfume = prompt ('how many perfume you want ?');
+var perfume = prompt ('How many perfume do you want ?');
  alert ('welcome to my shop');
  var img ='';
  for(var i=0; i<perfume ;i ++){
-     if(perfumeType == "si"){
-         document.write('<h3>'+ 'si perfume'+ '</h3>');
+     if(perfumeType == "Si"){
+         document.write('<h2>'+ 'Si perfume'+ '</h2>');
          img='<img src="https://www.sephora.com/productimages/sku/s2161008-av-01-zoom.jpg?imwidth=300">'
          document.write(img);
      
-        }else if(perfumeType== "one million"){
-         document.write('<h3>' + 'one million perfume'+'</h3>');
+        }else if(perfumeType== "One Million"){
+         document.write('<h2>' + 'One Million perfume'+'</h2>');
          img='<img src="https://d11zer3aoz69xt.cloudfront.net/media/catalog/product/cache/1/image/1200x/9df78eab33525d08d6e5fb8d27136e95/p/a/paco_rabanne_lady_million_edp_perfume_for_women_80ml_1_1.jpg">'
          document.write(img) 
      }
